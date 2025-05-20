3297ae244220e51bd8e37077ee99a8cc8469cc7a
Serge Latyntsev <dnsl48@gmail.com>
2025-05-20 18:01:26 +1200

    chore(example-nesting): share the nesting example tests

---
+++ example-nesting/src/lib.rs
@@ -0,0 +1,39 @@
+use struct_patch::Patch;
+
+#[derive(Clone, Debug, Patch)]
+pub struct TopItem {
+    pub id: u8,
+
+    #[patch(nesting)]
+    pub child_item: ChildItem
+}
+
+#[derive(Clone, Debug, Patch)]
+pub struct ChildItem {
+    pub id: u8
+}
+
+
+#[cfg(test)]
+mod tests {
+    use struct_patch::Patch;
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let item_a = TopItem {
+            id: 10,
+            child_item: ChildItem { id: 20 }
+        };
+
+        let item_b = TopItem {
+            id: 10,
+            child_item: ChildItem { id: 30 }
+        };
+
+        let diff = item_b.into_patch_by_diff(item_a);
+
+        assert!(diff.id.is_none());
+        assert_eq!(diff.child_item.id, Some(30));
+    }
+}
