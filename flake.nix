d22ea2a5a80d13ac73ced3322867d69f59c18dad
Antonio Yang <yanganto@gmail.com>
2025-12-03 11:41:31 +0800

    update 25.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11-small";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
     dependency-refresh.url = "github:yanganto/dependency-refresh";
