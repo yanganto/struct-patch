0d8c36f21c40508a53ab2b6812c1433306c85477
Antonio Yang <yanganto@gmail.com>
2026-05-28 18:59:09 +0800

    ci: use dependabot

--- flake.nix
+++ flake.nix
@@ -4,18 +4,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11-small";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    dependency-refresh.url = "github:yanganto/dependency-refresh";
   };
 
-  outputs = { self, rust-overlay, nixpkgs, flake-utils, dependency-refresh }:
+  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        dr = dependency-refresh.defaultPackage.${system};
-
         publishScript = pkgs.writeShellScriptBin "crate-publish" ''
           cargo login $1
           cargo publish -p struct-patch-derive || echo "publish struct-patch-derive fail"
@@ -27,22 +24,6 @@
           cd complex-example
           cargo test -p substrate
           cargo test -p catalyst
-        '';
-        updateDependencyScript = pkgs.writeShellScriptBin "update-dependency" ''
-          dr ./Cargo.toml
-
-          cd no-std-examples
-          dr ./Cargo.toml
-
-          cd ../complex-example
-          dr ./Cargo.toml
-
-          if [[ -f "Cargo.toml.old" || -f "no-std-examples/Cargo.toml.old" || -f "complex-example/Cargo.toml.old" ]]; then
-            rm -f Cargo.toml.old
-            rm -f no-std-examples/Cargo.toml.old
-            rm -f complex-example/Cargo.toml.old
-            exit 1
-          fi
         '';
       in
       with pkgs;
@@ -72,9 +53,7 @@
               openssl
               pkg-config
 
-              dr
               publishScript
-              updateDependencyScript
 
               checkCatalystScript
             ];
