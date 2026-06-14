8f943df10fe4786b7c61824bc745d5ed8041cd83
Antonio Yang <yanganto@gmail.com>
2026-06-14 11:55:48 +0800

    Revert "catalyst: init private struct test"
    This reverts commit d0facd4a7e0e5d52694407e6e807acd71392a1a9.

--- complex-example/catalyst/src/priv_struct.rs
+++
@@ -1,9 +0,0 @@
-//! This module demo how to use a Substrate from dependant crate
-//! In which some fields of Substrate and some field of Catalyst are private
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn complex_with_private_fields_works() {
-    }
-}
